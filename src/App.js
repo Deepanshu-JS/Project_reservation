7b0834408fc3d382684f63a648d9aa1456e5dac7
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-10-24 14:20:23 +0530

    first initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
+        <Route path="/hotels" element={<List/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../component/navbar/Navbar";
+import Header from "../../component/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  );
+};
+export default Home;
