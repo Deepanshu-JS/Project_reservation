e2217ae4da082b62aaeae6f1352c53cda7b8e3ef
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-10-24 16:16:00 +0530

    initial

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+
+import "./header.css";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerlist">
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airposrt taxi</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
