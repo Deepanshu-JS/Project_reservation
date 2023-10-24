69c316d03f16bccb4f977fb099610af7498bd637
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-10-25 01:19:55 +0530

    initial

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <div className="headerlist">
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
@@ -32,11 +32,19 @@ const Header = () => {
           </div>
           <div className="headerListItem">
             <FontAwesomeIcon icon={faTaxi} />
-            <span>Airposrt taxi</span>
+            <span>Airport taxi</span>
           </div>
         </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant saving of 10% or more
+          with a free booking Account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
       </div>
     </div>
+
+    
   );
 };
 
