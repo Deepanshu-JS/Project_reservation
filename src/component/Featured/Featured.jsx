7407fa41c04e1961161c5a75eb49e47a9a35b360
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-11-06 21:22:53 +0530

    initial

--- src/component/Featured/Featured.jsx
+++ src/component/Featured/Featured.jsx
@@ -3,7 +3,15 @@ import './Featured.css'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='featured'>
+        <div className='featuredItem'>
+          <img src='https://cf.bstatic.com/xdata/images/hotel/square600/476201495.webp?k=d7fd089b5c09afa8ec360cb5d187caa1e594e0156f49c21983a96c8f51971ee1&o='/>
+          <div className="featuredTitles">
+            <h1>Dublin</h1>
+            <h2>123 Properties</h2>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
- const Navbar = () => {
+const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className='navContainer'>
-          <span className='logo'>newBooking</span>
-          <div className='navItems '>
-            <button className='navButton'>Register</button>
-            <button className='navButton'>Login</button>
-          </div>
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">newBooking</span>
+        <div className="navItems ">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Navbar
+export default Navbar;
