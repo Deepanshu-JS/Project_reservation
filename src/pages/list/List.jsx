6d781171c43192a0b7eadfa32e23bb4d4ac23015
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-11-13 19:20:28 +0530

    Day 31

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../../component/navbar/Navbar";
 import Header from "../../component/header/Header";
+import "./list.css";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="ListContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
